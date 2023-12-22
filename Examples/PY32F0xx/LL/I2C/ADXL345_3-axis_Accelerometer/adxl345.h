79e451fdbe9fd7953d82f81f9a5153f1cf8cf8bc
IOsetting <iosetting@outlook.com>
2023-12-23 02:52:17 +0800

    feat: i2c adxl345 demo

---
+++ Examples/PY32F0xx/LL/I2C/ADXL345_3-axis_Accelerometer/adxl345.h
@@ -0,0 +1,176 @@
+// Copyright 2023 IOsetting <iosetting(at)outlook.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef __ADXL345_H
+#define __ADXL345_H
+
+#include "main.h"
+
+
+#define ADXL345_DEFAULT_ADDRESS     (0xA6) // 0xA6(SDO->GND), 0x3A(SDO->VCC)
+#define ADXL345_DEVICE_ID           (0xE5)
+
+#define ADXL345_REG_DEVID           (0x00) // R,   DEVID register holds a fixed device ID code of 0xE5 (345 octal)
+#define ADXL345_REG_THRESH_TAP      (0x1D) // R/W, Tap threshold
+#define ADXL345_REG_OFSX            (0x1E) // R/W, X-axis offset, The value stored in the offset registers is 
+                                           //   automatically added to the acceleration data, and the resulting 
+                                           //   value is stored in the output data registers
+#define ADXL345_REG_OFSY            (0x1F) // R/W, Y-axis offset
+#define ADXL345_REG_OFSZ            (0x20) // R/W, Z-axis offset
+#define ADXL345_REG_DUR             (0x21) // R/W, Tap duration
+#define ADXL345_REG_LATENT          (0x22) // R/W, Tap latency
+#define ADXL345_REG_WINDOW          (0x23) // R/W, Tap window
+#define ADXL345_REG_THRESH_ACT      (0x24) // R/W, Activity threshold
+#define ADXL345_REG_THRESH_INACT    (0x25) // R/W, Inactivity threshold
+#define ADXL345_REG_TIME_INACT      (0x26) // R/W, Inactivity time
+#define ADXL345_REG_ACT_INACT_CTL   (0x27) // R/W, Axis enable control for activity and inactivity detection
+#define ADXL345_REG_THRESH_FF       (0x28) // R/W, Free-fall threshold
+#define ADXL345_REG_TIME_FF         (0x29) // R/W, Free-fall time
+#define ADXL345_REG_TAP_AXES        (0x2A) // R/W, Axis control for single/double tap
+#define ADXL345_REG_ACT_TAP_STATUS  (0x2B) // R,   Source for single/double tap
+#define ADXL345_REG_BW_RATE         (0x2C) // R/W, Data rate and power mode control
+#define ADXL345_REG_POWER_CTL       (0x2D) // R/W, Power-saving features control
+#define ADXL345_REG_INT_ENABLE      (0x2E) // R/W, Interrupt enable control
+#define ADXL345_REG_INT_MAP         (0x2F) // R/W, Interrupt mapping control
+#define ADXL345_REG_INT_SOURCE      (0x30) // R,   Source of interrupts
+#define ADXL345_REG_DATA_FORMAT     (0x31) // R/W, Data format control
+#define ADXL345_REG_DATAX0          (0x32) // R,   X-axis data 0
+#define ADXL345_REG_DATAX1          (0x33) // R,   X-axis data 1
+#define ADXL345_REG_DATAY0          (0x34) // R,   Y-axis data 0
+#define ADXL345_REG_DATAY1          (0x35) // R,   Y-axis data 1
+#define ADXL345_REG_DATAZ0          (0x36) // R,   Z-axis data 0
+#define ADXL345_REG_DATAZ1          (0x37) // R,   Z-axis data 1
+#define ADXL345_REG_FIFO_CTL        (0x38) // R/W, FIFO control
+#define ADXL345_REG_FIFO_STATUS     (0x39) // R,   FIFO status
+
+#define ADXL345_MG2G_MULTIPLIER     (0.004) // 4mg per lsb
+
+/**
+ * Interrupt bits
+*/
+#define ADXL345_INT_DATA_READY      (0x80)
+#define ADXL345_INT_SINGLE_TAP      (0x40)
+#define ADXL345_INT_DOUBLE_TAP      (0x20)
+#define ADXL345_INT_ACTIVITY        (0x10)
+#define ADXL345_INT_INACTIVITY      (0x08)
+#define ADXL345_INT_FREE_FALL       (0x04)
+#define ADXL345_INT_WATERMARK       (0x02)
+#define ADXL345_INT_OVERRUN         (0x01)
+
+#define ADXL345_TAP_DETECT_AXIS_Z   (0x01)
+#define ADXL345_TAP_DETECT_AXIS_Y   (0x02)
+#define ADXL345_TAP_DETECT_AXIS_X   (0x04)
+
+/**
+ * @brief Output Data Rate, Used with register 0x2C (ADXL345_REG_BW_RATE) to set bandwidth.
+ *        The maximum output data rate when using 400 kHz I2C is 800 Hz and scales linearly 
+ *        with a change in the I2C communication speed. For example, using I2C at 100 kHz 
+ *        would limit the maximum ODR to 200 Hz
+*/
+typedef enum {
+    ADXL345_DATARATE_3200_HZ  = 0x0F, // 1600Hz Bandwidth
+    ADXL345_DATARATE_1600_HZ  = 0x0E, //  800Hz Bandwidth
+    ADXL345_DATARATE_800_HZ   = 0x0D, //  400Hz Bandwidth
+    ADXL345_DATARATE_400_HZ   = 0x0C, //  200Hz Bandwidth, 90 µA
+    ADXL345_DATARATE_200_HZ   = 0x0B, //  100Hz Bandwidth, 60 µA
+    ADXL345_DATARATE_100_HZ   = 0x0A, //   50Hz Bandwidth, 50 µA (default)
+    ADXL345_DATARATE_50_HZ    = 0x09, //   25Hz Bandwidth, 45 µA
+    ADXL345_DATARATE_25_HZ    = 0x08, // 12.5Hz Bandwidth, 40 µA
+    ADXL345_DATARATE_12_5_HZ  = 0x07, // 6.25Hz Bandwidth, 34 µA
+    ADXL345_DATARATE_6_25HZ   = 0x06, // 3.13Hz Bandwidth
+    ADXL345_DATARATE_3_13_HZ  = 0x05, // 1.56Hz Bandwidth
+    ADXL345_DATARATE_1_56_HZ  = 0x04, // 0.78Hz Bandwidth
+    ADXL345_DATARATE_0_78_HZ  = 0x03, // 0.39Hz Bandwidth
+    ADXL345_DATARATE_0_39_HZ  = 0x02, // 0.20Hz Bandwidth
+    ADXL345_DATARATE_0_20_HZ  = 0x01, // 0.10Hz Bandwidth
+    ADXL345_DATARATE_0_10_HZ  = 0x00, // 0.05Hz Bandwidth
+} ADXL345_DataRate_t;
+
+
+/**
+ * @brief  Used with register ADXL345_REG_DATA_FORMAT to set SPI wires
+ */
+typedef enum {
+    ADXL345_SELF_TEST_OFF    = 0x00,
+    ADXL345_SELF_TEST_ON     = 0x80,
+} ADXL345_SelfTest_t;
+
+/**
+ * @brief  Used with register ADXL345_REG_DATA_FORMAT to set SPI wires
+ */
+typedef enum {
+    ADXL345_SPI_WIRE_4  = 0x00,
+    ADXL345_SPI_WIRE_3  = 0x40,
+} ADXL345_SPI_Wire_t;
+
+/**
+ * @brief  Used with register ADXL345_REG_DATA_FORMAT to set interrupt active level
+ */
+typedef enum {
+    ADXL345_INT_ACTIVE_HIGH = 0x00,
+    ADXL345_INT_ACTIVE_LOW  = 0x20,
+} ADXL345_IntActive_t;
+
+/**
+ * @brief  Used with register ADXL345_REG_DATA_FORMAT to set resolution mode
+ */
+typedef enum {
+    ADXL345_DATA_RESOLVE_10BIT   = 0x00,
+    ADXL345_DATA_RESOLVE_FULL    = 0x08,
+} ADXL345_DataResolve_t;
+
+/**
+ * @brief  Used with register ADXL345_REG_DATA_FORMAT to set data alignment
+ */
+typedef enum {
+    ADXL345_DATA_ALIGNMENT_RIGHT = 0x00,
+    ADXL345_DATA_ALIGNMENT_LEFT  = 0x04,
+} ADXL345_DataAlignment_t;
+
+/**
+ * @brief  Used with register ADXL345_REG_DATA_FORMAT to set g range
+ */
+typedef enum {
+    ADXL345_G_RANGE_2G   = 0x00, // +/- 2g (default)
+    ADXL345_G_RANGE_4G   = 0x01, // +/- 4g
+    ADXL345_G_RANGE_8G   = 0x02, // +/- 8g
+    ADXL345_G_RANGE_16G  = 0x03, // +/- 16g
+} ADXL345_G_Range_t;
+
+uint8_t ADXL345_ReadByte(uint8_t addr);
+void ADXL345_BurstRead(uint8_t addr, uint8_t *pBuf, uint8_t size);
+
+void ADXL345_WriteByte(uint8_t addr, uint8_t dat);
+
+ErrorStatus ADXL345_Init(
+    ADXL345_DataRate_t dataRate,
+    ADXL345_SPI_Wire_t spiWire,
+    ADXL345_IntActive_t intLevel,
+    ADXL345_DataResolve_t resolve,
+    ADXL345_DataAlignment_t alignment,
+    ADXL345_G_Range_t range);
+/**
+ * Enable interrupts
+*/
+void ADXL345_SetInterrupts(uint8_t interrupts);
+/**
+ * Remap interrupts to INT2 (default is INT1)
+*/
+void ADXL345_RemapInterrupts(uint8_t interrupts);
+
+uint8_t ADXL345_IsInterrupt(uint8_t interrupt);
+
+void ADXL345_EnableTapDetectOnAxes(uint8_t axes);
+
+#endif // __ADXL345_H
---
+++ Examples/PY32F0xx/LL/I2C/ADXL345_3-axis_Accelerometer/main.c
@@ -0,0 +1,273 @@
+/***
+ * Demo: ADXL345 3-axis accelerometer
+ * 
+ *               CS       ---> 3.3V
+ * PA4   ------> INT1
+ * PA5   ------> INT2
+ *               SDO      ---> GND or 3.3V
+ * PF0   ------> SDI/SDA
+ * PF1   ------> SCL
+ * 
+ * PA2   ------> TX
+ * PA3   ------> RX
+ */
+#include <string.h>
+#include "main.h"
+#include "py32f0xx_bsp_printf.h"
+#include "py32f0xx_bsp_clock.h"
+#include "adxl345.h"
+
+#define MASTER_ADDRESS      0xA0
+
+#define I2C_STATE_READY    0
+#define I2C_STATE_BUSY_TX  1
+#define I2C_STATE_BUSY_RX  2
+
+__IO uint32_t   i2cState  = I2C_STATE_READY;
+
+
+__IO uint8_t count_int1 = 0, count_int2 = 0, count_double_tap = 0;
+__IO int16_t xbuf[3];
+
+static void APP_GPIOConfig(void);
+static void APP_I2CConfig(void);
+
+int main(void)
+{
+  BSP_RCC_HSI_PLL48MConfig();
+
+  BSP_USART_Config(115200);
+  printf("I2C Demo: ADXL345, 3-axis accelerometer\r\nClock: %ld\r\n", SystemCoreClock);
+
+  APP_GPIOConfig();
+  APP_I2CConfig();
+
+  if (ADXL345_Init(
+      ADXL345_DATARATE_100_HZ,
+      ADXL345_SPI_WIRE_4,
+      ADXL345_INT_ACTIVE_LOW,
+      ADXL345_DATA_RESOLVE_FULL,
+      ADXL345_DATA_ALIGNMENT_RIGHT,
+      ADXL345_G_RANGE_8G
+  ) == SUCCESS)
+  {
+    printf("ADXL345_Init success\r\n");
+  }
+  else
+  {
+    printf("ADXL345_Init failed\r\n");
+  }
+  // Tap threshold: 62.5mg / LSB, value = 2.5g / 0.0625g = 0x28
+  ADXL345_WriteByte(ADXL345_REG_THRESH_TAP, 0x28);
+  // Tap duration: 625us / LSB, value = 0.02s / 0.000625s = 0x20
+  ADXL345_WriteByte(ADXL345_REG_DUR, 0x20);
+  // Tap latency: 1.25ms / LSB, value = 0.1s / 0.00125s = 0x50
+  ADXL345_WriteByte(ADXL345_REG_LATENT, 0x50);
+  // Tap window: 1.25ms / LSB, value = 0.3s / 0.00125s = 0xF0
+  ADXL345_WriteByte(ADXL345_REG_WINDOW, 0xF0);
+  ADXL345_EnableTapDetectOnAxes(
+      ADXL345_TAP_DETECT_AXIS_X|ADXL345_TAP_DETECT_AXIS_Y|ADXL345_TAP_DETECT_AXIS_Z);
+  // Remap DATA_READY to INT2
+  ADXL345_RemapInterrupts(ADXL345_INT_DATA_READY);
+  ADXL345_SetInterrupts(
+      ADXL345_INT_DATA_READY|ADXL345_INT_SINGLE_TAP|ADXL345_INT_DOUBLE_TAP);
+  /*
+   * The interrupt functions are latched and cleared by either reading the data registers 
+   * (Address 0x32 to Address 0x37) until the interrupt condition is no longer valid for 
+   * the data-related interrupts or by reading the INT_SOURCE register (Address 0x30) for 
+   * the remaining interrupts.
+   */
+  ADXL345_BurstRead(ADXL345_REG_DATAX0, (uint8_t *)xbuf, 6);
+
+  while (1)
+  {
+    printf("X:%6d, Y:%6d, Z:%6d, DAT:%3d, TAP:%3d, 2-TAP:%3d\r\n",
+           xbuf[0], xbuf[1], xbuf[2], count_int2, count_int1, count_double_tap);
+    LL_mDelay(50);
+  }
+}
+
+static void APP_GPIOConfig(void)
+{
+  LL_GPIO_InitTypeDef GPIO_InitStruct;
+  LL_EXTI_InitTypeDef EXTI_InitStruct;
+
+  /* Enable peripheral clock */
+  LL_IOP_GRP1_EnableClock(LL_IOP_GRP1_PERIPH_GPIOA);
+
+  // PA6 CSN, set it to high to enable I2C mode
+  LL_GPIO_SetPinMode(GPIOA, LL_GPIO_PIN_6, LL_GPIO_MODE_OUTPUT);
+  LL_GPIO_SetOutputPin(GPIOA, LL_GPIO_PIN_6);
+  /* PA4 INT1 */
+  GPIO_InitStruct.Pin = LL_GPIO_PIN_4;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
+  LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  // PA5 INT2
+  GPIO_InitStruct.Pin = LL_GPIO_PIN_5;
+  LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* Triggerred by falling edge */
+  EXTI_InitStruct.Line = LL_EXTI_LINE_4 | LL_EXTI_LINE_5;
+  EXTI_InitStruct.LineCommand = ENABLE;
+  EXTI_InitStruct.Mode = LL_EXTI_MODE_IT;
+  EXTI_InitStruct.Trigger = LL_EXTI_TRIGGER_FALLING;
+  LL_EXTI_Init(&EXTI_InitStruct);
+
+  NVIC_SetPriority(EXTI4_15_IRQn, 1);
+  NVIC_EnableIRQ(EXTI4_15_IRQn);
+}
+
+static void APP_I2CConfig(void)
+{
+  LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  LL_IOP_GRP1_EnableClock(LL_IOP_GRP1_PERIPH_GPIOF);
+  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_I2C1);
+
+  // PF1 SCL
+  GPIO_InitStruct.Pin = LL_GPIO_PIN_1;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_HIGH;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_OPENDRAIN;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
+  GPIO_InitStruct.Alternate = LL_GPIO_AF_12;
+  LL_GPIO_Init(GPIOF, &GPIO_InitStruct);
+
+  // PF0 SDA
+  GPIO_InitStruct.Pin = LL_GPIO_PIN_0;
+  GPIO_InitStruct.Alternate = LL_GPIO_AF_12;
+  LL_GPIO_Init(GPIOF, &GPIO_InitStruct);
+
+  LL_APB1_GRP1_ForceReset(LL_APB1_GRP1_PERIPH_I2C1);
+  LL_APB1_GRP1_ReleaseReset(LL_APB1_GRP1_PERIPH_I2C1);
+
+  LL_I2C_InitTypeDef I2C_InitStruct;
+  /*
+   * Clock speed:
+   * - standard = 100khz
+   * - fast     = 400khz
+  */
+  I2C_InitStruct.ClockSpeed      = LL_I2C_MAX_SPEED_FAST;
+  I2C_InitStruct.DutyCycle       = LL_I2C_DUTYCYCLE_16_9;
+  I2C_InitStruct.OwnAddress1     = MASTER_ADDRESS;
+  I2C_InitStruct.TypeAcknowledge = LL_I2C_NACK;
+  LL_I2C_Init(I2C1, &I2C_InitStruct);
+}
+
+void APP_I2C_Transmit(uint8_t devAddress, uint8_t memAddress, uint8_t *pData, uint16_t size)
+{
+  while (i2cState == I2C_STATE_BUSY_TX);
+  LL_I2C_DisableBitPOS(I2C1);
+
+  i2cState = I2C_STATE_BUSY_TX;
+  /* Start */
+  LL_I2C_GenerateStartCondition(I2C1);
+  while (LL_I2C_IsActiveFlag_SB(I2C1) != 1);
+  /* Send slave address */
+  LL_I2C_TransmitData8(I2C1, (devAddress & (uint8_t)(~0x01)));
+  while(LL_I2C_IsActiveFlag_ADDR(I2C1) != 1);
+  LL_I2C_ClearFlag_ADDR(I2C1);
+
+  /* Send memory address */
+  while(LL_I2C_IsActiveFlag_TXE(I2C1) != 1);
+  LL_I2C_TransmitData8(I2C1, memAddress);
+  while(LL_I2C_IsActiveFlag_TXE(I2C1) != 1);
+
+  /* Transfer data */
+  while (size > 0)
+  {
+    while (LL_I2C_IsActiveFlag_TXE(I2C1) != 1);
+    LL_I2C_TransmitData8(I2C1, *pData++);
+    size--;
+
+    if ((LL_I2C_IsActiveFlag_BTF(I2C1) == 1) && (size != 0U))
+    {
+      LL_I2C_TransmitData8(I2C1, *pData++);
+      size--;
+    }
+
+    while (LL_I2C_IsActiveFlag_BTF(I2C1) != 1);
+  }
+
+  /* Stop */
+  LL_I2C_GenerateStopCondition(I2C1);
+
+  i2cState = I2C_STATE_READY;
+}
+
+uint8_t APP_I2C_Receive(uint16_t devAddress, uint16_t memAddress, uint8_t *buf, uint16_t size)
+{
+  uint8_t temp = 0;
+
+  i2cState    = I2C_STATE_BUSY_RX;
+  /* Turn on ACK */
+  LL_I2C_AcknowledgeNextData(I2C1, LL_I2C_ACK);
+  /* Start */
+  LL_I2C_GenerateStartCondition(I2C1);
+  /* Wait the status of Start Bit */
+  while(LL_I2C_IsActiveFlag_SB(I2C1) != 1);
+
+  /* Send slave address */
+  LL_I2C_TransmitData8(I2C1, (devAddress & (uint8_t)(~0x01)));
+  /* Wait the status of Address sent (master mode) */
+  while(LL_I2C_IsActiveFlag_ADDR(I2C1) != 1);
+  /* Clear Address Matched flag */
+  LL_I2C_ClearFlag_ADDR(I2C1);
+
+  /* Send memory address */
+  LL_I2C_TransmitData8(I2C1, (uint8_t)(memAddress & 0x00FF));
+  while (LL_I2C_IsActiveFlag_BTF(I2C1) != 1);
+
+  /* Start */
+  LL_I2C_GenerateStartCondition(I2C1);
+  while(LL_I2C_IsActiveFlag_SB(I2C1) != 1);
+
+  /* Send slave address(read) */
+  LL_I2C_TransmitData8(I2C1, (devAddress | 0x1));
+  while(LL_I2C_IsActiveFlag_ADDR(I2C1) != 1);
+  LL_I2C_ClearFlag_ADDR(I2C1);
+
+  while (size--)
+  {
+    while(LL_I2C_IsActiveFlag_RXNE(I2C1) != 1);
+    *buf++ = LL_I2C_ReceiveData8(I2C1);
+  }
+  LL_I2C_AcknowledgeNextData(I2C1, LL_I2C_NACK);
+  LL_I2C_GenerateStopCondition(I2C1);
+
+  i2cState = I2C_STATE_READY;
+  return temp;
+}
+
+
+void EXTI4_15_IRQHandler(void)
+{
+  if (LL_EXTI_IsActiveFlag(LL_EXTI_LINE_4)) // PA4 -> INT1
+  {
+    count_int1++;
+    if (ADXL345_IsInterrupt(ADXL345_INT_DOUBLE_TAP) > 0)
+    {
+        count_double_tap++;
+    }
+    LL_EXTI_ClearFlag(LL_EXTI_LINE_4);
+  }
+  else if (LL_EXTI_IsActiveFlag(LL_EXTI_LINE_5)) // PA5 -> INT2
+  {
+    count_int2++;
+    ADXL345_BurstRead(ADXL345_REG_DATAX0, (uint8_t *)xbuf, 6);
+    LL_EXTI_ClearFlag(LL_EXTI_LINE_5);
+  }
+}
+
+void APP_ErrorHandler(void)
+{
+  while (1);
+}
+
+#ifdef  USE_FULL_ASSERT
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  while (1);
+}
+#endif /* USE_FULL_ASSERT */

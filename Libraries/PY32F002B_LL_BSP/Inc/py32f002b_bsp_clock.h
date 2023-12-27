a142ee6616e3172a127203dd638b91df1f90948b
IOsetting <iosetting@outlook.com>
2023-12-27 22:49:30 +0800

    feat: 002b bsp - 24mhz hsi

---
+++ Libraries/PY32F002B_LL_BSP/Inc/py32f002b_bsp_clock.h
@@ -0,0 +1,32 @@
+/**
+  ******************************************************************************
+  * @file    py32f002b_bsp_clock.h
+  ******************************************************************************
+  */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef PY32F002B_BSP_CLOCK_H
+#define PY32F002B_BSP_CLOCK_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "py32f002b_ll_rcc.h"
+#include "py32f002b_ll_bus.h"
+#include "py32f002b_ll_system.h"
+#include "py32f002b_ll_exti.h"
+#include "py32f002b_ll_cortex.h"
+#include "py32f002b_ll_utils.h"
+#include "py32f002b_ll_pwr.h"
+#include "py32f002b_ll_gpio.h"
+#include "py32f002b_ll_usart.h"
+
+void BSP_RCC_HSI_24MConfig(void);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PY32F002B_BSP_CLOCK_H */
---
+++ Libraries/PY32F002B_LL_BSP/Src/py32f002b_bsp_clock.c
@@ -0,0 +1,24 @@
+#include "py32f002b_bsp_clock.h"
+
+void BSP_RCC_HSI_24MConfig(void)
+{
+  /*  Set FLASH Latency Before modifying the HSI */
+  LL_FLASH_SetLatency(LL_FLASH_LATENCY_0);
+  /* SET HSI to 24MHz */
+  LL_RCC_HSI_SetCalibTrimming(LL_RCC_HSICALIBRATION_24MHz);
+  /* Enable HSI */
+  LL_RCC_HSI_Enable();
+
+  while(LL_RCC_HSI_IsReady() != 1);
+  /* Set AHB divider: HCLK = SYSCLK */
+  LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+  /* HSISYS used as SYSCLK clock source  */
+  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_HSISYS);
+  while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_HSISYS);
+
+  /* Set APB1 divider */
+  LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
+  LL_Init1msTick(24000000);
+  /* Update CMSIS variable (which can be updated also through SystemCoreClockUpdate function) */
+  LL_SetSystemCoreClock(24000000);
+}
